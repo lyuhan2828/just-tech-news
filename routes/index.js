b9c607418ce38062e7079d3b1c7464c38d8fc716
Anna Liang <yuhan28283@gmail.com>
2022-06-06 22:08:31 -0400

    tested code

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+// Much like the API folder's index.js file that collects the endpoints and prefixes them, here we are collecting the packaged group of API endpoints and prefixing them with the path /api.
+
+// The router instance in routes/index.js collected everything for us and packaged them up for server.js to use.
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;

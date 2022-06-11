7c4a663d012efcc198850151e92ae60b5ca5121c
Anna Liang <yuhan28283@gmail.com>
2022-06-11 14:19:42 -0400

    update

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -3,7 +3,6 @@ const { Post, User } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
-    console.log('======================');
     Post.findAll({
       // Query configuration
         attributes: ['id', 'post_url', 'title', 'created_at'],
